17e03c3ec84660fed97712d01d7c4f69f201350d
Copperfield <miemailito@gmail.com>
2015-01-27 21:06:34 +0100

    Rewrite opencv script

--- script/opencv.sh
+++ script/opencv.sh
@@ -1,22 +1,36 @@
-version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
-echo "Installing OpenCV" $version
-mkdir OpenCV
-cd OpenCV
-echo "Removing any pre-installed ffmpeg and x264"
-sudo apt-get -qq remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
-sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg
-echo "Downloading OpenCV" $version
-wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
-echo "Installing OpenCV" $version
-unzip OpenCV-$version.zip
-cd opencv-$version
+version=2.4.10
+# Based on https://www.raben.com/book/export/html/3
+echo installing developer enviroment
+sudo apt-get -y install build-essential cmake pkg-config
+echo Installing Image I/O libraries
+sudo apt-get -y install libjpeg62-dev libtiff4 libjasper
+echo Installing the GTK dev library
+sudo apt-get -y install  libgtk2.0-dev
+echo Installing Video I/O libraries
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+echo installing support for Firewire video cameras
+sudo apt-get -y install libdc1394-22-dev
+echo installing video streaming libraries
+sudo apt-get -y install libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev 
+echo installing the Python development environment and the Python Numerical library
+sudo apt-get -y install python-dev python-numpy
+echo installimg the parallel code processing library (the Intel tbb library)
+sudo apt-get -y install libtbb-dev
+echo installing the Qt dev library
+sudo apt-get -y install libqt4-dev
+echo make directory
+mkdir opencv
+cd opencv
+echo downloading opencv library
+wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/OpenCV-$version.tar.bz2
+echo Descompressing
+tar -xvf OpenCV-$version.tar.bz2
+echo build project
+cd OpenCV-$version
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j2
-sudo checkinstall
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-sudo ldconfig
-echo "OpenCV" $version "ready to be used"
-exit 0
+echo compile opencv
+make
+echo installing opencv 
+sudo make install
