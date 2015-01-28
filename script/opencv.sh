35dbdf0b754f223b1275a94f7581aedc34fdff99
Copperfield <miemailito@gmail.com>
2015-01-28 01:05:10 +0100

    Rewrite cv2 script WIP

--- script/opencv.sh
+++ script/opencv.sh
@@ -14,19 +14,21 @@ echo installing video streaming libraries
 sudo apt-get -y install libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev 
 echo installing the Python development environment and the Python Numerical library
 sudo apt-get -y install python-dev python-numpy
-echo installimg the parallel code processing library (the Intel tbb library)
+echo installimg the parallel code processing library. The Intel tbb library
 sudo apt-get -y install libtbb-dev
 echo installing the Qt dev library
 sudo apt-get -y install libqt4-dev
+echo Installing unzip
+sudo apt-get -y install unzip
 echo make directory
 mkdir opencv
 cd opencv
 echo downloading opencv library
-wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/OpenCV-$version.tar.bz2
+wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-$version.zip
 echo Descompressing
-tar -xvf OpenCV-$version.tar.bz2
+unzip opencv-$version.zip
 echo build project
-cd OpenCV-$version
+cd opencv-$version
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
@@ -34,3 +36,4 @@ echo compile opencv
 make
 echo installing opencv 
 sudo make install
+exit 0
